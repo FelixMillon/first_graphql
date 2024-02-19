2d18f54272303e676ee47699bbfbe8b3c73d1ef3
iangalmiche4 <galmicheian@gmail.com>
2024-02-19 14:31:33 +0100

    add graphQL

--- mon-app-rick-morty/src/main.js
+++ mon-app-rick-morty/src/main.js
@@ -1,6 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './assets/global.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import './assets/global.css';
+import apolloClient from './apollo/apollo-client.ts';
+import { DefaultApolloClient } from '@vue/apollo-composable';
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+
+app.provide(DefaultApolloClient, apolloClient);
+
+app.use(router).mount('#app');
--- mon-app-rick-morty/src/views/HomeView.vue
+++ mon-app-rick-morty/src/views/HomeView.vue
@@ -1,27 +1,51 @@
 <template>
   <div class="home-container page-content">
     <h1 class="home-title">Rick & Morty</h1>
-    <div class="questions-overview">
-      <h2 class="overview-title">Liste des Personnages</h2>
-      <ul class="questions-list">
-        <li v-for="item in items" :key="item.id">
-          <router-link :to="{ name: 'detail', params: { id: item.id }}">
-            <img :src="item.image" alt="" style="width: 50px; height: auto;"> {{ item.name }}
-          </router-link>
-        </li>
-      </ul>
+    <div v-if="loading">
+      Chargement...
+    </div>
+    <div v-else-if="error">
+      Une erreur est survenue.
+    </div>
+    <div v-else-if="result && result.characters">
+      <div class="questions-overview">
+        <h2 class="overview-title">Liste des Personnages</h2>
+        <ul class="questions-list">
+          <li v-for="character in result.characters.results" :key="character.id">
+            <router-link :to="{ name: 'detail', params: { id: character.id }}">
+              <img :src="character.image" alt="" style="width: 50px; height: auto;"> {{ character.name }}
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div v-else>
+      Aucun personnage trouvé.
     </div>
   </div>
 </template>
 
+
 <script>
-import rickAndMortyData from '@/assets/data.json';
-export default {
-  name: 'HomeView',
-  data() {
-    return {
-      items: rickAndMortyData.results
+import { useQuery } from '@vue/apollo-composable';
+import { gql } from '@apollo/client/core';
+
+const GET_CHARACTERS = gql`
+  query GetCharacters {
+    characters {
+      results {
+        id
+        name
+        image
+      }
     }
   }
+`;
+export default {
+  name: 'HomeView',
+  setup() {
+    const { result, loading, error } = useQuery(GET_CHARACTERS);
+    return { result, loading, error };
+  },
 }
 </script>
