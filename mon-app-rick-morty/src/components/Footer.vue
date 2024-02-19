25083a541c11d4b0052c13ffc4f9d7301745f965
Ilyes <ilyesb36130@gmail.com>
2024-02-19 14:39:07 +0100

    add style css homeview

--- mon-app-rick-morty/src/components/Footer.vue
+++ mon-app-rick-morty/src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <footer class="footer">
-      <p>&copy; {{ currentYear }} - graph-ql. Ian GALMICHE - Felix MILLON - Elone MACCIONI.</p>
+      <p>&copy; {{ currentYear }} - graph-ql. Ian GALMICHE - Felix MILLON - Elone MACCIONI - Ilyès BOULKRINAT</p>
     </footer>
   </template>
   
--- mon-app-rick-morty/src/views/HomeView.vue
+++ mon-app-rick-morty/src/views/HomeView.vue
@@ -1,29 +1,79 @@
 <template>
   <div class="home-container page-content">
     <h1 class="home-title">Rick & Morty</h1>
-    <div v-if="loading">
+    <div v-if="loading" class="loading-message">
       Chargement...
     </div>
-    <div v-else-if="error">
+    <div v-else-if="error" class="error-message">
       Une erreur est survenue.
     </div>
-    <div v-else-if="result && result.characters">
-      <div class="questions-overview">
-        <h2 class="overview-title">Liste des Personnages</h2>
-        <ul class="questions-list">
-          <li v-for="character in result.characters.results" :key="character.id">
-            <router-link :to="{ name: 'detail', params: { id: character.id }}">
-              <img :src="character.image" alt="" style="width: 50px; height: auto;"> {{ character.name }}
-            </router-link>
-          </li>
-        </ul>
+    <div v-else-if="result && result.characters" class="questions-overview">
+      <h2 class="overview-title">Liste des Personnages</h2>
+      <div class="character-grid">
+        <div v-for="character in result.characters.results" :key="character.id" class="character-item">
+          <router-link :to="{ name: 'detail', params: { id: character.id }}">
+            <img :src="character.image" alt="" class="character-image">
+            <p class="character-name">{{ character.name }}</p>
+          </router-link>
+        </div>
       </div>
     </div>
-    <div v-else>
+    <div v-else class="no-characters-message">
       Aucun personnage trouvé.
     </div>
   </div>
 </template>
+
+<style scoped>
+.home-container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+.home-title {
+  font-size: 24px;
+  margin-bottom: 20px;
+}
+
+.loading-message, .error-message, .no-characters-message {
+  font-size: 18px;
+  margin-top: 20px;
+  text-align: center;
+}
+
+.questions-overview {
+  margin-top: 20px;
+}
+
+.overview-title {
+  font-size: 20px;
+  margin-bottom: 10px;
+}
+
+.character-grid {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.character-item {
+  width: calc(33.33% - 20px); /* 3 items per row with some margin between them */
+  margin-bottom: 20px;
+  margin-right: 20px;
+  box-sizing: border-box;
+}
+
+.character-image {
+  width: 100%;
+  height: auto;
+}
+
+.character-name {
+  text-align: center;
+  margin-top: 10px;
+}
+</style>
+
 
 
 <script>
